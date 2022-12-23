c5e29e6a5f111c853104b88b785f5a2504bb6a46
Achmad Qizwini <akise.shun@gmail.com>
2022-12-23 19:04:17 +0700

    update factory

--- factory/factory.go
+++ factory/factory.go
@@ -9,6 +9,14 @@ import (
 	userRepo "ikuzports/features/user/repository"
 	userService "ikuzports/features/user/service"
 
+	clubDelivery "ikuzports/features/club/delivery"
+	clubRepo "ikuzports/features/club/repository"
+	clubService "ikuzports/features/club/service"
+
+	clubMemberDelivery "ikuzports/features/clubMember/delivery"
+	clubMemberRepo "ikuzports/features/clubMember/repository"
+	clubMemberService "ikuzports/features/clubMember/service"
+
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
 )
@@ -21,4 +29,13 @@ func InitFactory(e *echo.Echo, db *gorm.DB) {
 	authRepoFactory := authRepo.New(db)
 	authServiceFactory := authService.New(authRepoFactory)
 	authDelivery.New(authServiceFactory, e)
+
+	clubMemberRepoFactory := clubMemberRepo.New(db)
+	clubMemberServiceFactory := clubMemberService.New(clubMemberRepoFactory)
+	clubMemberDelivery.New(clubMemberServiceFactory, e)
+
+	clubRepoFactory := clubRepo.New(db)
+	clubServiceFactory := clubService.New(clubRepoFactory, clubMemberRepoFactory)
+	clubDelivery.New(clubServiceFactory, e)
+
 }
