7024d6a84b934aed365bb867148dce66932264f7
Achmad Qizwini <akise.shun@gmail.com>
2022-12-29 12:40:37 +0700

    update message responses

--- features/user/delivery/request.go
+++ features/user/delivery/request.go
@@ -8,10 +8,8 @@ type InsertRequest struct {
 	Password    string `json:"password" form:"password"`
 	PhoneNumber string `json:"phone_number" form:"phone_number"`
 	Address     string `json:"address" form:"address"`
-	City        string `json:"city" form:"city"`
 	UserImage   string `json:"user_image" form:"user_image"`
 	Gender      string `json:"gender" form:"gender"`
-	Biodata     string `json:"biodata" form:"biodata"`
 }
 
 func toCore(userInput InsertRequest) user.Core {
@@ -21,10 +19,8 @@ func toCore(userInput InsertRequest) user.Core {
 		Password:    userInput.Password,
 		PhoneNumber: userInput.PhoneNumber,
 		Address:     userInput.Address,
-		City:        userInput.City,
 		UserImage:   userInput.UserImage,
 		Gender:      userInput.Gender,
-		Biodata:     userInput.Biodata,
 	}
 	return userCoreData
 }
