7d78c7be2a846c52710f033fae6ce6cec3f3ff97
Achmad Qizwini <akise.shun@gmail.com>
2022-12-29 22:00:53 +0700

    add page responses

--- utils/helper/responses.go
+++ utils/helper/responses.go
@@ -21,3 +21,12 @@ func SuccessWithDataResponse(msg string, data interface{}) map[string]interface{
 		"data":    data,
 	}
 }
+
+func SuccessWithPageDataResponse(msg string, data interface{}, page int) map[string]interface{} {
+	return map[string]interface{}{
+		"status":     "Success",
+		"message":    msg,
+		"data":       data,
+		"total_page": page,
+	}
+}
