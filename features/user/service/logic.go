c27f8af9f444bad2fdd928ea912849a95e125337
Achmad Qizwini <akise.shun@gmail.com>
2022-12-30 23:28:55 +0700

    update validation

--- features/user/service/logic.go
+++ features/user/service/logic.go
@@ -94,8 +94,19 @@ func (service *userService) GetById(id int) (data user.Core, err error) {
 func (service *userService) Update(input user.Core, id int) error {
 
 	if input.Password != "" {
+		str := helper.ValidatePassword(input.Password)
+		if str != "Valid" {
+			return errors.New(str)
+		}
 		generate, _ := bcrypt.GenerateFromPassword([]byte(input.Password), 10)
 		input.Password = string(generate)
+	}
+
+	if input.Email != "" {
+		errEmailFormat := helper.EmailFormatValidation(input.Email)
+		if errEmailFormat != nil {
+			return errors.New(errEmailFormat.Error())
+		}
 	}
 	// validasi email harus unik pas update, kalau email nya sama dgn punya dia gpp
 	data, errFindEmail := service.userRepository.FindUser(input.Email)
@@ -103,15 +114,13 @@ func (service *userService) Update(input user.Core, id int) error {
 		return errors.New("Failed. Email " + input.Email + " already exist at other user. Please pick another email.")
 	}
 
-	if errFindEmail != nil { //ketika error = email belum ada, proses berjalan
+	if errFindEmail != nil || data.Email == input.Email { //ketika error = email belum ada, atau ketika input email yg sama proses berjalan
 		err := service.userRepository.Update(input, id)
 		if err != nil {
 			log.Error(err.Error())
 			return helper.ServiceErrorMsg(err)
 		}
-
 	}
-
 	return nil
 }
 
